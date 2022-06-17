9268e4ff428d4a820614ceb3e4b07ea89e4e52bb
Ismaelleon <Ismaelleon@protonmail.com>
2022-06-16 23:54:52 -0300

    build: move client folder to 'server/src'
    Move the client folder to 'server/src' so the server can serve the
    client build.

--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,7 @@ const express = require('express'),
 const app = express();
 
 // middlewares
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
