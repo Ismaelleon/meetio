60b0b608ca34c19f8e7736a7c472f0b59d2e9b10
Ismaelleon <Ismaelleon@protonmail.com>
2022-06-26 02:25:44 -0300

    feat: add text to navigation bar

--- server/src/client/src/routes/components/NavigationBar.js
+++ server/src/client/src/routes/components/NavigationBar.js
@@ -8,12 +8,30 @@ function NavigationBar () {
 	return (
 		<nav>
 			<ul>
-				<li><Link to="/home"><MaterialIcon color={location.pathname === '/home' ? "#ff005c" : ""} icon="public" size={34} /></Link></li>
-				<li><Link to="/matches"><MaterialIcon color={
-					location.pathname === '/matches' || 
-					location.pathname === '/likes' ||
-					location.pathname === '/dislikes' ? "#ff005c" : ""} icon="favorite" size={34} /></Link></li>
-				<li><Link to="/profile"><MaterialIcon color={location.pathname === '/profile' ? "#ff005c" : ""} icon="person" size={34} /></Link></li>
+				<li>
+					<Link to="/home">
+						<MaterialIcon color={location.pathname === '/home' ? "#ff005c" : ""} icon="public" size={26} />
+						<span style={{color: location.pathname === '/home' ? "#ff005c" : ""}}>Home</span>
+					</Link>
+				</li>
+				<li>
+					<Link to="/matches">
+						<MaterialIcon color={
+							location.pathname === '/matches' || 
+							location.pathname === '/likes' ||
+							location.pathname === '/dislikes' ? "#ff005c" : ""} icon="favorite" size={26} />
+						<span style={{color: 
+							location.pathname === '/matches' || 
+							location.pathname === '/likes' ||
+							location.pathname === '/dislikes' ? "#ff005c" : ""}}>Interactions</span>
+					</Link>
+				</li>
+				<li>
+					<Link to="/profile">
+						<MaterialIcon color={location.pathname === '/profile' ? "#ff005c" : ""} icon="person" size={26} />
+						<span style={{color: location.pathname === '/profile' ? "#ff005c" : ""}}>Profile</span>
+					</Link>
+				</li>
 			</ul>
 		</nav>
 	);
