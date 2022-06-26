17e40a1e0e8dcde3a3d66ac13673e3548a111774
Ismaelleon <Ismaelleon@protonmail.com>
2022-06-26 00:08:36 -0300

    feat: enable https

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,15 @@
+const fs = require('fs'),
+	http = require('http'),
+	https = require('https');
+
 const app = require('./app');
 
-let port = process.env.PORT || 8080;
-
 if (process.env.NODE_ENV !== 'test') {
-	app.listen(port, () => {
-		console.log(`app running on port ${port}`)
-	})
+	let httpServer = http.createServer(app);
+	let httpsServer = https.createServer(app);
+
+	httpServer.listen(8080, () => console.log('http on port 8080'));
+	httpsServer.listen(8443, () => console.log('https on port 8443'));
+
 }
 
