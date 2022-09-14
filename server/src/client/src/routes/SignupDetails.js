e3040ae8d51c5c7e3ccd988928b11b7ae2983327
Ismaelleon <Ismaelleon@protonmail.com>
2022-09-14 06:14:05 -0300

    feat: add client functionality to crop the avatar image
    Create a dialog in /signup-details, to crop the avatar image (using react-avatar-editor), this dialog
    will send a request to /profile/crop-avatar; using jimp, crop the avatar and save it.

--- server/src/client/src/routes/SignupDetails.js
+++ server/src/client/src/routes/SignupDetails.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import MaterialIcon from 'material-icons-react';
+import FileToBase64 from 'dd-file-to-base64';
 import './stylesheets/form.css';
 
+import AvatarCropper from './components/AvatarCropper';
+
 function SignupDetails () {
-	let [avatar, setAvatar] = useState(''),
+	let [avatarFileName, setAvatarFileName] = useState(''),
+		[avatarBase64, setAvatarBase64] = useState(''),
+		[dialogVisible, setDialogVisible] = useState(false),
 		[description, setDescription] = useState('');
 
 	let fileInput = React.createRef(),
@@ -13,18 +18,19 @@ function SignupDetails () {
 
 	let history = useHistory();
 
-
 	function submitSignUp (event) {
 		event.preventDefault();
 
-		let formData = new FormData();
-
-		formData.append('avatar', fileInput.current.files[0])
-		formData.append('description', description)
+		let body = JSON.stringify({
+			description
+		});
 
 		fetch('/api/signup-details', {
 			method: 'POST',
-			body: formData
+			body,
+			headers: {
+				'Content-Type': 'application/json'
+			}
 		}).then(res => {
 			if (res.status === 200) {
 				history.push('/home', false)
@@ -32,18 +38,42 @@ function SignupDetails () {
 		})
 	}
 
-
 	function updateForm (event) {
-		if (avatar !== '') {
+		if (avatarFileName !== '') {
 			button.current.classList.add('clickable');
 
-			avatarView.current.src = URL.createObjectURL(fileInput.current.files[0])
+			avatarView.current.src = '';
+			avatarView.current.src = `/avatars/${avatarFileName}`;
 		} else {
+			if (fileInput.current.files.length > 0) {
+				avatarView.current.src = URL.createObjectURL(fileInput.current.files[0])
+			}
 			button.current.classList.remove('clickable');
 		}
 	}
 
-	useEffect(updateForm, [avatar])
+	function hideDialog () {
+		setDialogVisible(false)
+	}
+
+	useEffect(updateForm, [avatarFileName])
+
+	function uploadAvatar () {
+		let formData = new FormData();
+
+		formData.append('avatar', fileInput.current.files[0])
+
+		fetch('/api/profile/change-avatar', {
+			method: 'POST',
+			body: formData
+		}).then(res => res.json())
+		.then(avatar => {
+			setAvatarFileName(avatar)
+		})
+		.catch(error => console.log(error))	
+
+		hideDialog()
+	}
 
 	return(
 		<div>
@@ -64,8 +94,15 @@ function SignupDetails () {
 						ref={fileInput}
 						accept=".png, .jpg, .jpeg, .gif"
 						name="avatar"
-						onChange={event => {
-							setAvatar(event.target.files[0].name);
+						onChange={async event => {
+							if (event.target.files.length > 0) {
+								let file = event.target.files[0];
+								let base64Image = await FileToBase64.convert(file);
+
+								uploadAvatar()
+								setAvatarBase64(base64Image)
+								setDialogVisible(true)
+							}
 						}} />
 					<input type="text"
 						name="description"
@@ -74,6 +111,7 @@ function SignupDetails () {
 					<button ref={button} onClick={submitSignUp}>Send</button>
 				</form>
 			</main>
+			<AvatarCropper fileInput={fileInput} avatarView={avatarView} avatarBase64={avatarBase64} visible={dialogVisible} hideDialog={hideDialog} setAvatarFileName={setAvatarFileName} />
 		</div>
 	);
 }
