4786c6fa1cabe6ea5f8e2d5538396e5d617a3965
Ismaelleon <Ismaelleon@protonmail.com>
2022-12-09 02:37:46 -0300

    feat: add button to like or dislike a user in /user

--- server/src/client/src/routes/User.js
+++ server/src/client/src/routes/User.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import LoadingBar from 'react-top-loading-bar';
 import { useLocation } from 'react-router-dom';
-import { MdClear, MdFavorite, MdVerified } from 'react-icons/md';
+import { MdArrowDropDown, MdClear, MdFavorite, MdVerified } from 'react-icons/md';
 
 import ProfileLoader from './components/ProfileLoader';
 
 function User (props) {
-	let [profileData, setProfileData] = useState({});
+	let [user, setUser] = useState({});
+	let [dropdownVisible, setDropdownVisible] = useState(false);
 	let [loading, setLoading] = useState(true);
 
 	let [progress, setProgress] = useState(20);
@@ -27,7 +28,28 @@ function User (props) {
 			setProgress(100)
 			
 			setLoading(false)
-			setProfileData(data)
+			setUser(data)
+		})
+	}
+
+	function tapUser (event) {
+		event.stopPropagation()
+
+		let body = {
+			name: user.name,
+			like: event.currentTarget.classList[0] === 'like'
+		};
+
+		body = JSON.stringify(body);
+
+		fetch('/api/home/tap', {
+			method: 'POST',
+			body: body,
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).then(res => {
+			getUserData()
 		})
 	}
 
@@ -59,19 +81,30 @@ function User (props) {
 			<main>
 				<div className="profile">
 					<div className="avatar">
-						{profileData.avatar !== undefined ?
-							<img src={`/avatars/${profileData.avatar}`} alt={`${profileData.name}'s avatar`} />
+						{user.avatar !== undefined ?
+							<img src={`/avatars/${user.avatar}`} alt={`${user.name}'s avatar`} />
 							: <></>}
 					</div>
 					<span>
 						<h2>
-							{profileData.name}
+							{user.name}
 						</h2>
-						{profileData.verified ? <span><MdVerified fontSize="24px" color="rgb(0, 122, 255)" /></span> : <span></span>}
+						{user.verified ? <span><MdVerified fontSize="24px" color="rgb(0, 122, 255)" /></span> : <span></span>}
 					</span>
-		 			<p>{profileData.description}</p>
+		 			<p>{user.description}</p>
+					<button onClick={() => setDropdownVisible(!dropdownVisible)}>
+						{user.liked ?
+							<span>Liked <MdFavorite fontSize="var(--h4)" color="#ff005c" /></span> : 
+							<span style={{ color: '#009e5c' }}>Disliked <MdClear fontSize="var(--h4)" color="#099e5c !important" /></span>}
+						
+						<MdArrowDropDown fontSize="var(--h4)" color="#000" />
+					</button>
+					<ul style={ dropdownVisible ? { display: 'block' } : { display: 'none' } }>
+						<li onClick={tapUser} className="like">Like <MdFavorite fontSize="var(--h4)" color="#ff005c" /></li>
+						<li onClick={tapUser} className="dislike">Dislike <MdClear fontSize="var(--h4)" color="#ff005c" /></li>
+					</ul>
 		 			<div className="pictures">
-						{profileData.pictures !== undefined ? profileData.pictures.map((picture, index) =>
+						{user.pictures !== undefined ? user.pictures.map((picture, index) =>
 							<figure key={index}>
 								<img src={`/pictures/${picture}`}  alt="Profile photos" />
 							</figure>
