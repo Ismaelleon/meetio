ec8a65da4b4ebaa67641ec0bcb0d43e07553625c
Ismaelleon <Ismaelleon@protonmail.com>
2023-06-06 01:13:46 -0300

    fix: set the right size for the loader components

--- src/client/src/routes/components/HomeLoader.js
+++ src/client/src/routes/components/HomeLoader.js
@@ -6,19 +6,23 @@ function HomeLoader (props) {
 		[height, setHeight] = useState(0);
 
 	function setWindowSize () {
-		setWidth(window.innerWidth)
-		setHeight(window.innerHeight)
+		setWidth(window.innerWidth);
+		setHeight(window.innerHeight);
+
+		if (window.innerWidth > 512) {
+			setWidth(512);
+		}
 	}
 
 	useEffect(setWindowSize, [])
 
 	return (
-		<ContentLoader width={width} height={(height * 70) / 100} viewBox={`0 0 ${width} ${(height * 70) / 100}`} 
+		<ContentLoader width={width} height={height} style={{ margin: 'auto' }} viewBox={`0 0 ${width} ${height}`} 
 			backgroundColor="#d9d9d9" foregroundColor="#ecebeb" {...props}>
-			<rect x="0" y="0" width={`${width}`} height={`${height / 2}`} />
-			<rect x="20" y={`${(height / 2) + 25}`} width="170" height="25" />
-			<rect x="20" y={`${(height / 2) + 55}`} width={`${width - 40}`} height="20" />
-			<rect x="20" y={`${(height / 2) + 80}`} width={`${width - 40}`} height="20" />
+			<rect x="0" y="52" width={`${width}`} height={`${height / 2}`} />
+			<circle cx="75" cy={`${height / 2 + 52}`} r="55" />
+			<rect x="20" y={`${(height / 2 + 52) + 75}`} width="100" height="25" />
+			<rect x="20" y={`${(height / 2 + 52) + 110}`} width="200" height="20" />
 		</ContentLoader>
 	);
 }
